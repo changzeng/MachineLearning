3dd9db9c5c47b699223c531d912e57b1b9c60cb5
Changzeng <1207338829@qq.com>
2017-06-23 19:20:58 +0800

    new version

--- Adaboost/adaboost.py
+++ Adaboost/adaboost.py
@@ -1,4 +1,5 @@
-from numpy import matrix
+from numpy import *
+from boost import buildStump
 
 def loadSimpData():
     data_mat = matrix([[1.  ,  2.1],
@@ -9,3 +10,34 @@ def loadSimpData():
     class_label = [1.0, 1.0, -1.0, -1.0, 1.0]
 
     return data_mat,class_label
+
+def adaBoostTrainDS(data_arr,class_labels,num_it=40):
+	weak_class_arr = []
+	m = shape(data_arr)[0]
+	D = mat(ones((m,1))/m)
+	agg_class_est = mat(zeros((m,1)))
+
+	for i in range(num_it):
+		best_stump,error,class_est = buildStump(data_arr,class_labels,D)
+		# print("D:",D.T)
+		alpha = float(0.5*log((1.0-error)/max(error,1e-16)))
+		best_stump['alpha'] = alpha
+		weak_class_arr.append(best_stump)
+		# print("classEst: ",class_est.T)
+		expon = multiply(-1*alpha*mat(class_labels).T,class_est)
+		D = multiply(D,exp(expon))
+		D = D/D.sum()
+		agg_class_est += alpha*class_est
+		# print("aggClassEst: ",agg_class_est.T)
+		agg_errors = multiply(sign(agg_class_est) != mat(class_labels).T,ones((m,1)))
+		error_rate = agg_errors.sum()/m
+
+		print("total error: ",error_rate)
+
+		if error_rate == 0.0:
+			break
+
+	return weak_class_arr
+
+data_mat,class_labels = loadSimpData()
+weak_class_arr = adaBoostTrainDS(data_mat,class_labels)
--- Adaboost/boost.py
+++ Adaboost/boost.py
@@ -9,3 +9,43 @@ def stumpClassify(data_matrix, dimen, thresh_val, thresh_ineq):
 		ret_array[data_matrix[:,dimen] > thresh_val] = -1.0
 
 	return ret_array
+
+def buildStump(data_arr, class_labels, D):
+	data_matrix = mat(data_arr)
+	label_mat = mat(class_labels).T
+
+	m,n = shape(data_matrix)
+	num_steps = 10.0
+	best_stump = {}
+	best_clas_est = mat(zeros((m,1)))
+	min_error = inf
+
+	for i in range(n):
+		range_min = data_matrix[:,i].min()
+		range_max = data_matrix[:,i].max()
+
+		step_size = (range_max-range_min)/num_steps
+		for j in range(-1,int(num_steps)+1):
+			for inequal in ['lt','gt']:
+				thresh_val = (range_min + float(j)*step_size)
+				predicted_vals = stumpClassify(data_matrix,i,thresh_val,inequal)
+
+				err_arr = mat(ones((m,1)))
+				# array filtrate
+				err_arr[predicted_vals == label_mat] = 0
+				weighted_error = D.T * err_arr
+				# print("split: dim %d, thresh %.2f,thresh inequal: %s, the weighted error is %.3f" % (i, thresh_val, inequal, weighted_error))
+
+				if weighted_error < min_error:
+					min_error = weighted_error
+					best_clas_est = predicted_vals.copy()
+					best_stump['dim'] = i
+					best_stump['thresh'] = thresh_val
+					best_stump['ineq'] = inequal
+
+	return best_stump,min_error,best_clas_est
+
+# from adaboost import loadSimpData
+# data_mat,class_labels = loadSimpData()
+# D = mat(ones((5,1))/5)
+# buildStump(data_mat,class_labels,D)
